893df2e8faa43fed71a3444d39dafdd1d9351561
unknown <kos0360@gmail.com>
2014-09-24 11:21:50 +0300

    created struct link

---
+++ link.cpp
@@ -0,0 +1,34 @@
+// Example program
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Link
+{
+    Link(const string& v)
+    : value(v), succ(0), first(this){}
+
+    Link* add(Link*);
+    Link* add_ordered(Link*);
+    Link* find(const string& v);
+    const Link* find(const string& v) const;
+    Link* next();
+    
+    string value;
+    Link* succ;
+};
+
+
+
+int main()
+{
+    
+    Link* w new Link("What");
+    
+    
+  std::string name;
+  std::cout << "What is your name? ";
+  getline (std::cin, name);
+  std::cout << "Hello, " << name << "!\n";
+}
